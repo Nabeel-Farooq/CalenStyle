1055c7c072d2afa87dcf9205b314c588c2a29a18
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-04-28 21:08:08 +0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,10 @@ module.exports = function(grunt)
 {
 	var sBanner = '/* ----------------------------------------------------------------------------- ' +
 	'\n\n  CalenStyle - Responsive Event Calendar' +
-	'\n  Version <%= pkg.version %>' + 
+	'\n  Version <%= pkg.version %>' +  
 	'\n  Copyright (c)<%= grunt.template.today("yyyy") %> Lajpat Shah' +
-	'\n  Contributors : https://github.com/nehakadam/CalenStyle/contributors' +
-	'\n  Repository : https://github.com/nehakadam/CalenStyle' +
+	'\n  Contributors : https://github.com/nehakadam/CalenStyle/contributors' + 
+	'\n  Repository : https://github.com/nehakadam/CalenStyle' + 
 	'\n  Homepage : https://nehakadam.github.io/CalenStyle' +
 	'\n\n ----------------------------------------------------------------------------- */\n\n';
 	
